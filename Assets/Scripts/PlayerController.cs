ebce0ee8ad21a0728434f86f5c068965d8177439
Tristan2002 <71229711+Tristan2002@users.noreply.github.com>
2023-05-22 14:24:19 +0200

    Upudateu

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -68,6 +68,7 @@ public class PlayerController : MonoBehaviour
 
         if (Input.GetKey(KeyCode.Space) & canJump)
         {
+            Console.WriteLine("Jump");
             rb.AddForce(0f, jumpSpeed * Time.deltaTime, 0f);
         }
     }
